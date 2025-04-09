f11b8413f03573d035a4118ece9e8b27e93f7437
kamoliddin <kamoliddin@gmail.com>
2025-04-09 17:31:15 +0500

    partfolyu

---
+++ js/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    title: "intro-section-with-dropdown-navigation",
+    github: "https://github.com/jexaweb/2oy-7dars-uyga-vazifa",
+    vercel: "https://2oy-7dars-uyga-vazifa.vercel.app/",
+  },
+  {
+    title: "social-links-profile--Card",
+    github: "https://github.com/jexaweb/7-uyga-vazifa",
+    vercel: "https://7-uyga-vazifa.vercel.app/",
+  },
+  {
+    title: "blog-preview-card--Card",
+    github: "https://github.com/jexaweb/7dars-uyga-vazifa1",
+    vercel: "https://7dars-uyga-vazifa1.vercel.app/",
+  },
+];
